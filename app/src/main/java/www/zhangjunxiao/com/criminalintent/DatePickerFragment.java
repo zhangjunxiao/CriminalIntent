658f00e884afa6ada47481db3344d6255e5a5993
zhangjunxiao <291722629@qq.com>
2017-05-31 17:46:05 +0800

    datePicker

---
+++ app/src/main/java/www/zhangjunxiao/com/criminalintent/DatePickerFragment.java
@@ -0,0 +1,26 @@
+package www.zhangjunxiao.com.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/5/31.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date,null);
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok,null)
+                .create();
+    }
+}
